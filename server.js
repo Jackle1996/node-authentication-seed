a518d4e4ed55cd91d58f9824dd2b5495b4b97d8f
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-06 13:56:20 +0200

    simple web authentication

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const server = require('express')();
+const basicAuth = require('express-basic-auth');
 
 function main() {
-  let server = express();
-
-  // TODO: Basic Auth
+  
+  server.use(basicAuth({
+      users: { 'admin1': 'supersecret' },    
+      challenge: true
+  }));
 
   const port = process.env.PORT || 8000;
   server.listen(port, () => console.log(`Server is listening on port: ${port}`));
